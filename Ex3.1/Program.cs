94a4681464b6666323e7eb582d5e70fc0908b155
Souleraser <souleraser030791@gmail.com>
2021-08-03 17:19:51 +0300

    1

---
+++ Ex3.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ex3._1 //// Написать программу, которая подсчитывает расстояние между точками с координатами x1, y1 и x2,y2
+                 //// по формуле r=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2). 
+                 ///Вывести результат, используя спецификатор формата .2f (с двумя знаками после запятой);
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите координату x1 = ");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите координату y1 = ");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите координату x2 = ");
+            int x2 = int.Parse(Console.ReadLine());
+            Console.Write("Введите координату y2 = ");
+            int y2 = int.Parse(Console.ReadLine());
+            double r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Расстояние между точками {0:f}", r);
+            
+        }
+    }
+}
---
+++ Ex5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ex5 ////  Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+              ////  б) Сделать задание, только вывод организовать в центре экрана.
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.Write("Напишите ваше имя:");
+            String name = Console.ReadLine();
+
+            Console.Write("Напишите вашу фамилию:");
+            String secondname = Console.ReadLine();
+
+            Console.Write("Напишите ваш город ипроживания:");
+            String city = Console.ReadLine();
+            Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2);
+
+
+            Console.WriteLine(name + " " + secondname + " г. " + city);
+                    Console.ReadKey();
+
+
+
+
+
+
+
+
+
+                }
+            }
+        }
